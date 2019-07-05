6c97e67d93f677e385dcfed897d0bc6a98cda2d4
radosik <radik.052005@gmail.com>
2019-07-05 18:34:22 +0200

    relize1

--- src/js/play.js
+++ src/js/play.js
@@ -1,6 +1,9 @@
-import {Human} from "./human";
+import Human from "./human";
 
-const w = window.innerWidth;
+const person1 = new Human(200,180,50);
+
+
+/*const w = window.innerWidth;
 const h = window.innerHeight;
 
 const canvas = document.getElementById("tablet");
@@ -9,6 +12,29 @@ canvas.setAttribute('height', h - 2);
 
 const ctx = tablet.getContext('2d');
 
-const modelOne = new Human(100,100,180);
+const modelOne = new Human(ctx,200,500,200);
+let save = modelOne.draw();
+let deg = 0;
+let time;
 
 
+modelOne.setKnee(90);
+save = modelOne.draw(save);
+
+
+
+time = setInterval(function(){
+    modelOne.setKnee(deg);
+    save = modelOne.draw(save);
+    deg++;
+    if (deg > 2)
+        clearInterval(time);
+},1000);*/
+
+/*time = setInterval(function(){
+    modelOne.setKnee(deg);
+    save = modelOne.draw(save);
+    deg--;
+    if (deg < 1)
+    clearInterval(time);
+},400);*/
