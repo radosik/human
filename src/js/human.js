3d5df7b904917a77e9000a9daffdf1d524f9baab
web-ramos <webmaster.ramos@gmail.com>
2019-07-05 21:06:55 +0200

    Ramos fixes

--- src/js/human.js
+++ src/js/human.js
@@ -6,224 +6,161 @@ import { setTimeout } from "timers";
 class Human {
  
     constructor(x,y,l) {
-
         this.bodyParts = {
             head: {
                 id: null,
+                name: 'head',
                 x: x,
-                y: y - 130,
-                r: 50,
-                a: Math.PI * 2,
+                y: y - l * 3.5,
+                xEnd: 0,
+                yEnd: 0,
+                l: l,
+                a: 0,
+                child: []
             },
+
             body: {
                 id: null,
-                x: x,
-                y: y - 100,
-                l: l + 50,
-            }, 
-            larm: {
-                id: null,
-                x: x,
-                y: y - 20,
-                l: l + 30,
-            }, 
-            rarm: {
-                id: null,
-                x: x,
-                y: y - 20,
-                l: l + 30,
-            }, 
-            lleg: {
-                id: null,
+                name: 'body',
                 x: x,
                 y: y,
-                l: l - 10,
-                a: 0,
-            }, 
-            lknee: {
-                id: null,
-                x: x,
-                y: y - 120,
-                l: l,
-                a: 0,
-            }, 
+                xEnd: x,
+                yEnd: y - l * 3.5,
+                l: l * 3.5,
+                a: 180,
+                child: ['head']
+            },
+
             rleg: {
                 id: null,
+                name: 'rightLeg',
                 x: x,
                 y: y,
-                l: l - 10,
+                xEnd: x,
+                yEnd: y + l * 1.5,
+                l: l * 1.5,
                 a: 0,
+                child: ['rknee']
             },
+
             rknee: {
                 id: null,
+                name: 'rightKnee',
                 x: x,
-                y: y - 120,
-                l: l,
+                y: y + l * 1.5,
+                xEnd: x,
+                yEnd: y + l * 3.5,
+                l: l * 2,
                 a: 0,
+                child: []
+            },
+
+            lleg: {
+                id: null,
+                name: 'leftLeg',
+                x: x,
+                y: y,
+                xEnd: x,
+                yEnd: y + l * 1.5,
+                l: l * 1.5,
+                a: 0,
+                child: ['lknee']
+            },
+
+            lknee: {
+                id: null,
+                name: 'leftKnee',
+                x: x,
+                y: y + l * 1.5,
+                xEnd: x,
+                yEnd: y + l * 3.5,
+                l: l * 2,
+                a: -25,
+                child: []
             }
+
         };
+
+        self = this;
 
         const field = document.getElementById('field');
         const div = document.createElement('div');
         const humanTag = field.appendChild(div);
 
+        // init contexts
         for(let value in this.bodyParts) {
-            console.log(this.bodyParts[value].y);
             let el = document.createElement('canvas');
             let child = humanTag.appendChild(el);
+
+            child.width = window.innerWidth;
+            child.height = window.innerHeight;            
+
             this.bodyParts[value].id = child.getContext('2d');
-            console.log(this.bodyParts[value].id);
             let ctx = this.bodyParts[value].id;
-            console.log(ctx);
-            
+
+            ctx.width = window.innerWidth;
+            ctx.height = window.innerHeight;
+        }
+
+        // draw person
+        for(let value in this.bodyParts) {  
+
+            let ctx = this.bodyParts[value].id;
 
             if (value === 'head') {
                 ctx.beginPath();
-                ctx.arc(200, 50, 30, 0, Math.PI * 2, true);
-                ctx.stroke();
+                ctx.arc(this.bodyParts[value].x, this.bodyParts[value].y - this.bodyParts[value].l/2, this.bodyParts[value].l/2, 0, Math.PI * 2, true);
+                ctx.fillStyle = "blue";
+                ctx.fill();
             } else {
-                ctx.beginPath();
-                ctx.moveTo(this.bodyParts[value].x, this.bodyParts[value].y);
-                ctx.lineTo(this.bodyParts[value].x, this.bodyParts[value].y + this.bodyParts[value].l);
-                ctx.stroke();
+                this.drawPart(this.bodyParts[value]);
             }
+        }
+    }
 
-            
-            console.log(this.bodyParts[value].id);
+    drawPart (value) {
+
+        const ctx = value.id;
+        let x,y;
+        ctx.clearRect(0, 0, ctx.width, ctx.height);
+        ctx.beginPath();
+        ctx.moveTo(value.x, value.y);
+        x = Math.sin(value.a * Math.PI / 180) * value.l + value.x;
+        y = Math.cos(value.a * Math.PI / 180) * value.l + value.y;        
+
+        // has been changed?
+        if (x !== value.xEnd  && y !== value.yEnd) {
+            value.xEnd = x;
+            value.yEnd = y;
+            for (let i = 0; i < value.child.length; i++) {
+                this.bodyParts[value.child[i]].x = value.xEnd;
+                this.bodyParts[value.child[i]].y = value.yEnd;
+                this.drawPart(this.bodyParts[value.child[i]]);
+            }
         }
 
-
-        /*const ctx2 = document.createElement('canvas');
-        const ctx2Tag = div.appendChild(ctx2);
-        const ctx3 = document.createElement('canvas');
-        const ctx3Tag = div.appendChild(ctx3);
-        const ctx4 = document.createElement('canvas');
-        const ctx4Tag = div.appendChild(ctx4);
-        const ctx5 = document.createElement('canvas');
-        const ctx5Tag = div.appendChild(ctx5);
-        const ctx6 = document.createElement('canvas');
-        const ctx6Tag = div.appendChild(ctx6);*/
-
-
-        /*const ctx1 = head.getContext('2d');
-        const ctx2 = body.getContext('2d');
-        const ctx3 = larm.getContext('2d');
-        const ctx4 = rarm.getContext('2d');
-        const ctx5 = lleg.getContext('2d');
-        const ctx6 = rleg.getContext('2d');*/
-        console.log(field);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();                
+        ctx.arc(value.x, value.y, 5, 0, Math.PI * 2, true);
+        ctx.fillStyle = "blue";
+        ctx.fill();    
     }
+
+    drawStep (value, goal, sign = true) {
+        const mult = sign ? 1: -1;
+
+        const timerId = setInterval(function() {
+            self.drawPart(self.bodyParts[value]);
+            self.bodyParts[value].a += 2 * mult;
+            if (self.bodyParts[value].a * mult > goal * mult) {
+                clearInterval(timerId);
+                self.drawStep ('lleg',-25,false);                
+            }
+        },50);        
+    }
+
 }
-
-
-
-/*let human = () => {
-    let head = () => {
-        ctx1.beginPath();
-        ctx1.arc(200, 50, 30, 0, Math.PI * 2, true);
-        ctx1.stroke();
-    } 
-
-    let body = () => {
-        ctx2.beginPath();
-        ctx2.strokeStyle = "#00ff00";
-        ctx2.moveTo(200, 80);
-        ctx2.lineTo(200, 180);
-        ctx2.stroke();
-        
-    }
-
-    let larm = () => {
-        ctx3.beginPath();
-        ctx3.strokeStyle = "#ff0000";
-        ctx3.moveTo(200, 160);
-        ctx3.lineTo(200, 130);
-        ctx3.stroke();
-        
-    }
-
-    let rarm = () => {
-        ctx4.beginPath();
-        ctx4.strokeStyle = "blue";
-        ctx4.moveTo(200, 160);
-        ctx4.lineTo(200, 130);
-        ctx4.stroke();
-        
-    } 
-
-    let lleg = () => {
-        ctx5.beginPath();
-        ctx5.strokeStyle = "black";
-        ctx5.moveTo(200, 180);
-        ctx5.lineTo(200, 280);
-        ctx5.stroke();
-        
-    }
-
-    let rleg = () => {
-        ctx6.beginPath();
-        ctx6.strokeStyle = "orange";
-        ctx6.moveTo(200, 180);
-        ctx6.lineTo(200, 280);
-        ctx6.stroke();
-        
-    } 
-
-    let step = (ctx) => {
-        console.log('start');         
-        const l = 50;
-        let angle = 0;
-
-            const timerId = setInterval(function() {
-                let x = Math.sin(angle) * l + 200;
-                let xx = Math.sin(angle) * l + 212;
-                let y = Math.cos(angle) * l + 180;
-                let yy = Math.cos(angle) * l + 228;  
-                ctx.clearRect(0, 0, 1000, 1000);
-                ctx.beginPath();
-                ctx.moveTo(200, 180);
-                ctx.lineTo(x, y);
-                ctx.lineTo(x, yy);
-                ctx.stroke();
-                const afterKnee = setTimeout(function() {
-                    ctx.clearRect(0, 0, 1000, 1000);
-                    ctx.beginPath();
-                    ctx.moveTo(200, 180);
-                    ctx.lineTo(212, 228);
-                    ctx.moveTo(212, 228);
-                    ctx.lineTo(xx, yy);
-                    ctx.stroke();
-                    if (angle > 0.26) {
-                        clearInterval(timerId);
-                    }
-                },1500);        
-                angle += 0.01;
-                if (angle > 0.26) {
-                    clearInterval(timerId);
-                    console.log(x);
-                    console.log(y);
-                    console.log(yy);                                           
-                }
-            },50);
-        console.log('completed'); 
-    }
-    
-   
-    head();
-    body();
-    larm();
-    rarm();
-    lleg();
-    rleg();
-
-
-    
-    setTimeout(step(ctx5), 200);
-
-    
-}*/
-
-
 
 export default Human;
 
